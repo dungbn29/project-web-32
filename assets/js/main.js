a31df0ff23a61f345a0acb2029dbcb653ab5c859
dungbn29 <tiendungchu298@gmail.com>
2023-02-09 17:22:51 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-});
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300: {
+          items: 1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+    
+  });
